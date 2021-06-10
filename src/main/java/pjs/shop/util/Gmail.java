5424f963a885b580abb3dbbe215eb6543f6241be
bavuchoko <38562113+bavuchoko@users.noreply.github.com>
2021-06-10 23:30:15 +0900

    Update Gmail.java

--- src/main/java/pjs/shop/util/Gmail.java
+++ src/main/java/pjs/shop/util/Gmail.java
@@ -7,6 +7,6 @@ public class Gmail extends Authenticator{
     
     @Override
     protected PasswordAuthentication getPasswordAuthentication(){
-        return new PasswordAuthentication("greatparkjs8501@gmail.com", "shjs*850105");
+        return new PasswordAuthentication("", "******");
     }
 }
